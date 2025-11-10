e5903487800144891438578e48975d60fd23016b
moradkdani308-beep <moradkdani308@gmail.com>
2025-11-10 06:19:45 -0500

    Primer commit del proyecto RecetarioIA

---
+++ app.py
@@ -0,0 +1,28 @@
+import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+
+# Cargar la clave del archivo .env
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+app = Flask(__name__)
+MODEL = "models/gemini-2.5-flash"
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_input = request.json.get("message", "")
+    try:
+        model = genai.GenerativeModel(MODEL)
+        response = model.generate_content(f"Responde como un chef y experto en nutrición: {user_input}")
+        return jsonify({"response": response.text})
+    except Exception as e:
+        return jsonify({"response": f"⚠️ Error: {e}"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
